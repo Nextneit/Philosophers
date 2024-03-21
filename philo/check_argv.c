9dc43374d6cc9de637ff2fa69987049138f9643c
Nestor Cruz Gambero <ncruz-ga@c2r1s6.42malaga.com>
2024-03-21 12:08:33 +0100

    init hilos

---
+++ philo/check_argv.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_argv.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 13:45:51 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/03/19 14:34:16 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static int	is_digit(char c)
+{
+	if (c >= 48 && c <= 57)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	check_argv(char **argv, int argc, int i, int j)
+{
+	if (argc != 5 && argc != 6)
+		return (printf("Error: Numero de argumentos invalido\n"),
+			EXIT_FAILURE);
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (ft_atoi(argv[i]) == 0)
+				return (printf("Error: Argumento invalido\n"), EXIT_FAILURE);
+			if (is_digit(argv[i][j]) == EXIT_FAILURE || ft_atoi(argv[i]) < 0)
+				return (printf("Error: Argumento incorrecto\n"),
+					EXIT_FAILURE);
+			else
+				j++;
+		}
+		i++;
+	}
+	if (ft_atoi(argv[1]) > 200)
+		return (printf("Error: Numero de filosofos incorrecto\n"),
+			EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
---
+++ philo/philo.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ga <ncruz-ga@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 11:27:20 by ncruz-ga          #+#    #+#             */
+/*   Updated: 2024/03/20 10:53:06 by ncruz-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static void	init_struct(t_data *d, t_philo *philos)
+{
+	d->dead_flag = 0;
+	d->philos = philos;
+	pthread_mutex_init(&d->dead_lock, NULL);
+	pthread_mutex_init(&d->write_lock, NULL);
+	pthread_mutex_init(&d->meal_lock, NULL);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data			*d;
+	t_philo			philos[200];
+	pthread_mutex_t	forks[200];
+
+	if (check_argv(argv, argc, 1, 0) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	d = malloc(sizeof(t_data *));
+	init_forks(forks, ft_atoi(argv[1]));
+	init_philos(philos, &d, forks, argv);
+	init_struct(d, philos);
+	init_thread(d, philos, ft_atoi(argv[1]));
+}
